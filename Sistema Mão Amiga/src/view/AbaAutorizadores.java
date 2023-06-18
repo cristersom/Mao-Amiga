42a38e67184f3061476ad7caf500e7192417ccb2
Eduardo Menzen <50517871+emenzen@users.noreply.github.com>
2023-06-18 20:44:00 -0300

    Update AbaAutorizadores.java teste de alteração

--- Sistema Mão Amiga/src/view/AbaAutorizadores.java
+++ Sistema Mão Amiga/src/view/AbaAutorizadores.java
@@ -24,37 +24,41 @@ public class AbaAutorizadores extends JPanel {
     
     public AbaAutorizadores() {
         this.setLayout(new BorderLayout());
-        final ArrayList dadosAutor = new ArrayList();
-        final String[] colunasAutor = { "Nome", "Telefone", "Tipo" };
-        final boolean[] edicaoAutor = { true, true, true };
-        this.modelo = new ModeloTabela(dadosAutor, colunasAutor, edicaoAutor);
-        this.tabela = new JTable((TableModel)this.modelo);
-        this.tabela.getColumnModel().getColumn(0).setPreferredWidth(40);
-        this.tabela.getColumnModel().getColumn(0).setResizable(true);
-        this.tabela.getColumnModel().getColumn(1).setPreferredWidth(50);
-        this.tabela.getColumnModel().getColumn(1).setResizable(true);
-        this.tabela.getColumnModel().getColumn(2).setPreferredWidth(10);
-        this.tabela.getColumnModel().getColumn(2).setResizable(true);
-        this.tabela.getTableHeader().setReorderingAllowed(false);
-        this.tabela.setAutoResizeMode(3);
-        this.tabela.setSelectionMode(0);
-        final JScrollPane rolagemTabelaAutor = new JScrollPane(this.tabela);
+        
+        ArrayList dadosAutor = new ArrayList();
+        String[] colunasAutor = { "Nome", "Telefone", "Tipo" };
+        boolean[] edicaoAutor = { true, true, true };
+        modelo = new ModeloTabela(dadosAutor, colunasAutor, edicaoAutor);
+        tabela = new JTable((TableModel)modelo);
+        
+        tabela.getColumnModel().getColumn(0).setPreferredWidth(40);
+        tabela.getColumnModel().getColumn(0).setResizable(true);
+        tabela.getColumnModel().getColumn(1).setPreferredWidth(50);
+        tabela.getColumnModel().getColumn(1).setResizable(true);
+        tabela.getColumnModel().getColumn(2).setPreferredWidth(10);
+        tabela.getColumnModel().getColumn(2).setResizable(true);
+        tabela.getTableHeader().setReorderingAllowed(false);
+        tabela.setAutoResizeMode(3);
+        tabela.setSelectionMode(0);
+        
+        JScrollPane rolagemTabelaAutor = new JScrollPane(tabela);
         this.add(rolagemTabelaAutor, "Center");
-        final JPanel pnlBottomAutor = new JPanel();
+        JPanel pnlBottomAutor = new JPanel();
         pnlBottomAutor.setBorder(BorderFactory.createBevelBorder(1));
-        final JLabel lblNomeAutor = new JLabel("Nome:");
+        JLabel lblNomeAutor = new JLabel("Nome:");
         pnlBottomAutor.add(lblNomeAutor);
-        pnlBottomAutor.add(this.txtNomeAutor = new JTextField());
-        this.txtNomeAutor.setColumns(35);
-        final JLabel lblTelefoneAutor = new JLabel("Telefone:");
+        pnlBottomAutor.add(txtNomeAutor = new JTextField());
+        txtNomeAutor.setColumns(35);
+        
+        JLabel lblTelefoneAutor = new JLabel("Telefone:");
         pnlBottomAutor.add(lblTelefoneAutor);
-        pnlBottomAutor.add(this.txtTelefoneAutor = new JTextField());
-        this.txtTelefoneAutor.setColumns(15);
-        (this.jcbTipo = new JComboBox<String>()).setToolTipText("Tipo");
-        this.jcbTipo.setModel(new DefaultComboBoxModel<String>(new String[] { "Busca", "Emerg\u00eancia" }));
-        pnlBottomAutor.add(this.jcbTipo);
-        pnlBottomAutor.add(this.btnIncluir = new JButton("Incluir"));
-        pnlBottomAutor.add(this.btnExcluir = new JButton("Excluir"));
+        pnlBottomAutor.add(txtTelefoneAutor = new JTextField());
+        txtTelefoneAutor.setColumns(15);
+        (jcbTipo = new JComboBox<String>()).setToolTipText("Tipo");
+        jcbTipo.setModel(new DefaultComboBoxModel<String>(new String[] { "Busca", "Emergência" }));
+        pnlBottomAutor.add(jcbTipo);
+        pnlBottomAutor.add(btnIncluir = new JButton("Incluir"));
+        pnlBottomAutor.add(btnExcluir = new JButton("Excluir"));
         this.add(pnlBottomAutor, "South");
     }
 }
